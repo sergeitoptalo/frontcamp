6043dda9c0e80d53d29884944d0d4ea7700517bd
MINSK\Siarhei_Taptala <toptalo@hotmail.com>
2017-11-23 17:54:33 +0300

    task02

--- task-01/scripts/containers/list.js
+++
@@ -1,6 +0,0 @@
-export function renderChannelList(container) {
-    let listElement = document.createElement('ul');
-    let list = container.appendChild(listElement);
-
-    return list;
-}
--- task-01/scripts/containers/listItem.js
+++
@@ -1,6 +0,0 @@
-export function renderListItem(list) {
-    let listItemElement = document.createElement('li');
-    let listItem = list.appendChild(listItemElement);
-
-    return listItem;
-}
