79dd499e4dda0b78c86b1157627bdeb04e598d1b
sergeitoptalo <toptalo@hotmail.com>
2017-11-25 22:28:44 +0300

    remove extra code

--- task-01/scripts/index.js
+++ task-01/scripts/index.js
@@ -6,9 +6,11 @@ import MenuToggle from './utilities/menuToggle';
 document.addEventListener('DOMContentLoaded', () => {
     let channelsListContainer = document.querySelector('#channels-list-container');
     let channelsList = renderChannelList(channelsListContainer);
+
     config.forEach(newsChannel => new NewsChannel(newsChannel).render(channelsList));
+
     let chooseChannelButton = document.querySelector('#choose-channel-button');
-    //addToggle();
     let toggles = document.querySelectorAll('[data-toggle-target]');
+    
     Array.from(toggles).forEach(toggle => new MenuToggle(toggle).createToggle())
 });
