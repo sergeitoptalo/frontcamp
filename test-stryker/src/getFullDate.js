2fdd0ffb9ced024d21c315b19fc118d6267bd86e
MINSK\Siarhei_Taptala <toptalo@hotmail.com>
2018-04-04 14:20:21 +0300

    add files

---
+++ test-stryker/src/getFullDate.js
@@ -0,0 +1,40 @@
+let addZeroesToTime = (date) => {
+    let givenDate = new Date(date);
+    let minutes = givenDate.getMinutes();
+    let hours = givenDate.getHours();
+    let result = ('0' + hours).slice(-2) + ':' + ('0' + minutes).slice(-2);
+
+    return result;
+}
+
+let monthNameFromNumber = (monthNumber) => {
+    const month = [
+        '',
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+
+    return month[monthNumber];
+}
+
+let getFullDate = (passedDate) => {
+    let date = new Date(passedDate);
+    let time = addZeroesToTime(date);
+    let resultDate = `${monthNameFromNumber(date.getMonth() + 1)} ${date.getDate()}`;
+    let resultTime = `${time}`;
+
+    return {
+        date: resultDate,
+        time: resultTime
+    };
+}
