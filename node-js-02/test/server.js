1350abd062f3af4c931c53930176b19b94d30a2c
MINSK\Siarhei_Taptala <toptalo@hotmail.com>
2018-04-04 18:08:37 +0300

    add tests

--- node-js-02/test/server.js
+++ node-js-02/test/server.js
@@ -2,20 +2,18 @@ let mongoose = require("mongoose");
 let Blog = require('../server/schema.js').Blog;
 const app = require('../server.js').app;
 const routes = require('../server/routes.js');
-//Require the dev-dependencies
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server.js');
 let should = chai.should();
 var expect = chai.expect;
 var sinon = require('sinon');
+var assert = require('assert');
 
 chai.use(chaiHttp);
 
-describe('/GET index', () => {
-    
-    it('it should GET all the blogs for the index page', (done) => {
-
+describe('Get index page', () => {
+    it('it should send index page', (done) => {
         chai.request(server)
             .get('/')
             .end((err, res) => {
@@ -25,35 +23,49 @@ describe('/GET index', () => {
     });
 });
 
-describe('Blogs', () => {
-    describe('/GET blogs', () => {
-        it('it should GET all the blogs on the route /blogs', (done) => {
-            chai.request(server)
-                .get('/blogs')
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('array');
-                    done();
-                });
-        });
+describe('Get all blogs', () => {
+    it('it should GET all the blogs on the route /blogs', (done) => {
+        chai.request(server)
+            .get('/blogs')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                done();
+            });
     });
 });
 
-describe('Blog', () => {
-    describe('/GET single blog', () => {
-        it('it should GET single blog on route /blogs/:id', (done) => {
-            chai.request(server)
-                .get('/blogs/5ac394fdb41e361198a28ee2')
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('object');
-                    
-                    done();
-                });
-        });
-        after(function () {
-            //server.close();
-            process.exit(0);
-        });
+describe('Get single blog', () => {
+    it('it should GET single blog on route /blogs/:id', (done) => {
+        chai.request(server)
+            .get('/blogs/5ac394fdb41e361198a28ee2')
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            });
     });
 });
+
+describe('Add blog', () => {
+    it('it should send success message if blog was added', (done) => {
+        let blog = {
+            title: "title 2",
+            author: "author 2",
+            text: 'text 2',
+            date: new Date()
+        }
+
+        chai.request(server)
+            .post('/add-blog')
+            .send(blog)
+            .end((err, res) => {
+                res.text.should.be.equal('added');
+                done();
+            });
+    });
+    
+    after(function () {
+        process.exit(0);
+    });
+});
+
