c0ca71f252db26bf1b58d1b287d9070ba69b6f26
MINSK\Siarhei_Taptala <toptalo@hotmail.com>
2018-04-10 12:38:29 +0300

    fix tests

--- react-part1/tests/components/LoginForm.test.js
+++ react-part1/tests/components/LoginForm.test.js
@@ -13,10 +13,11 @@ import LoginForm from '../../src/client/components/LoginForm';
 import Input from '../../src/client/components/formComponents/Input';
 
 describe('LoginForm Component', () => {
-    const loginForm = render(<Provider store={store}><BrowserRouter><LoginForm /></BrowserRouter></Provider>);
+    const loginFormMarkup = <Provider store={store}><BrowserRouter><LoginForm /></BrowserRouter></Provider>;
+    const loginForm = render(loginFormMarkup);
 
     it('should render without throwing an error', () => {
-        expect(shallow(<Provider store={store}><BrowserRouter><LoginForm /></BrowserRouter></Provider>).exists(<form className='app-form'></form>)).toBe(true)
+        expect(shallow(loginFormMarkup).exists(<form className='app-form'></form>)).toBe(true)
     })
     it('renders a email input', () => {
         expect(loginForm.find('#login').length).toEqual(1);
